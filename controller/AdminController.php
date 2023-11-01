6751d837d363cb8dbe45bbb2efd2c9e09148e8c4
Candela Fernandez <cande.fdz12@gmail.com>
2023-11-01 18:35:54 -0300

    lista partidas

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -13,16 +13,41 @@ class AdminController
         $this->model = $model;
         $this->renderer = $renderer;
     }
+    public function list() {
+        $this->renderer->render('admin');
+    }
      public function traerJugadores(){
-
-      $jugadores =  $this->model->TraerTodosLosJugadores();
-
+      $jugadores =  $this->model->traerTodosLosJugadores();
+      $totalUsuarios = count($jugadores);
          $datosJugadores = [
-             'jugadores' => $jugadores
+             'jugadores' => $jugadores,
+             'totalUsuarios' => $totalUsuarios
          ];
-
          $this->renderer->render('listadoJugadores', $datosJugadores);
      }
+     public function traerPartidas(){
+        //intentamos que se muestre el nombre del usuario en vez del id, pero no se muestra nada
+        $partidas = $this->model->traerTodasLasPartidas();
+        $nombresUsuarios = [];
+
+        foreach ($partidas as $partida) {
+            $idUsuario = $partida['idUsuario'];
+            $nombreUsuario = $this->model->traerNombrePorId($idUsuario);
+            $nombresUsuarios[$idUsuario] = $nombreUsuario;
+        }
+    
+        $totalPartidas = count($partidas);
+    
+        $datosPartidas = [
+            'partidas' => $partidas,
+            'totalPartidas' => $totalPartidas,
+            'nombresUsuarios' => $nombresUsuarios,
+        ];
+    
+        $this->renderer->render('listadoPartidas', $datosPartidas);
+       }
+      
+    
 
 
 
--- model/AdminModel.php
+++ model/AdminModel.php
@@ -9,10 +9,17 @@ class AdminModel
     {
         $this->database = $database;
     }
-    public function TraerTodosLosJugadores(){
-        $sql = "Select * from usuarios where idRol = 1  ";
+    public function traerTodosLosJugadores(){
+        $sql = "SELECT * FROM usuarios WHERE idRol = 1";
        return $this->database->query($sql);
-
+    }
+    public function traerTodasLasPartidas(){
+        $sql = "SELECT * FROM partida";
+       return $this->database->query($sql);
+    }
+    public function traerNombrePorId($idUsuario){
+        $sql = "SELECT nombre FROM usuarios WHERE id = $idUsuario";
+        return $this->database->query($sql);
     }
 
 }
